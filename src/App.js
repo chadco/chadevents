f6d7146dd32c5e9efec2fe1ba9b848981429cef7
Chad Comstock <chadco@gmail.com>
2020-12-28 13:06:07 -0700

    FREAKING WORK ALREAY

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Chad Comstock Man
         </a>
       </header>
     </div>
